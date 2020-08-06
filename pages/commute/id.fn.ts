c2703ce72fb44115e48e54e255b8b27ad0050a87
홍성준 <freevue@hongseongjun-ui-MacBookPro.local>
2020-08-07 08:53:50 +0900

    [test]: dayjs 시간대역 확인

--- pages/commute/id.fn.ts
+++ pages/commute/id.fn.ts
@@ -4,7 +4,6 @@ import 'dayjs/locale/ko'
 import { CommuteConstant, SnackConstant } from '~/constant'
 import { ICommuteItem } from '~/types'
 
-console.log(dayjs.locale())
 interface IState {
   modal: boolean
   loading: boolean
@@ -38,9 +37,12 @@ export const useComputed = ({ root }: SetupContext) =>
       const detailItem: ICommuteItem =
         root.$store.getters[`commute/${CommuteConstant.$Get.CommuteItem}`]
 
-      console.log(detailItem.startDate, dayjs(detailItem.startDate))
+      console.log('dayjs 시간대역', dayjs.locale())
+      console.log('API 순수 시간', detailItem.startDate)
+      console.log('dayjs에 입력', dayjs(detailItem.startDate))
+      console.log('dayjs에 포맷', dayjs(detailItem.startDate).format('HH:mm'))
       console.log(
-        dayjs(detailItem.startDate).format('HH:mm'),
+        'dayjs에 지역 시간대 지정 후 포맷',
         dayjs(detailItem.startDate).locale('ko').format('HH:mm')
       )
 
