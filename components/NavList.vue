ed1b8aae54deac10b0c44c30d3916f856ef31be9
freevue <hackish@hanmail.net>
2020-08-31 22:27:50 +0900

    [UX/UI]: 메뉴에 기본적인 사용자 UI 추가

---
+++ components/NavList.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-list>
+    <v-list-item v-for="(item, i) in state.items" :key="i" :to="item.to" router exact>
+      <v-list-item-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.title" />
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+import { defineComponent, reactive } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      items: [
+        {
+          icon: 'home',
+          title: '메인 페이지',
+          to: '/',
+        },
+        {
+          icon: 'timelapse',
+          title: '출퇴근 리스트',
+          to: '/commute',
+        },
+      ],
+    })
+
+    return {
+      state,
+    }
+  },
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,24 @@
 <template>
   <v-app dark>
     <v-navigation-drawer v-model="state.drawer" fixed app>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-title>사용자 이름</v-list-item-title>
+
+        <v-btn icon>
+          <v-icon>settings</v-icon>
+        </v-btn>
+      </v-list-item>
       <v-divider></v-divider>
-      <v-list>
-        <v-list-item v-for="(item, i) in state.items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <nav-list />
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn outlined block>Logout</v-btn>
+        </div>
+      </template>
     </v-navigation-drawer>
     <v-app-bar fixed app color="primary">
       <v-app-bar-nav-icon @click.stop="state.drawer = !state.drawer" />
@@ -31,29 +38,18 @@ import 'vuetify/dist/vuetify.min.css'
 
 import dayjs from 'dayjs'
 import { defineComponent, reactive, computed } from '@vue/composition-api'
-import { SnackBar, DefaultFooter } from '~/components'
+import { SnackBar, DefaultFooter, NavList } from '~/components'
 import { MainConstant } from '~/constant'
 
 export default defineComponent({
   components: {
     DefaultFooter,
     SnackBar,
+    NavList,
   },
   setup(_, context) {
     const state = reactive({
       drawer: false,
-      items: [
-        {
-          icon: 'home',
-          title: '메인 페이지',
-          to: '/',
-        },
-        {
-          icon: 'timelapse',
-          title: '출퇴근 리스트',
-          to: '/commute',
-        },
-      ],
     })
     const title = reactive({
       str: computed(() => {
