b53d8d38c3f8aa40f602b7cb076a9459d1711598
freevue <hackish113@gmail.com>
2020-09-01 23:12:18 +0900

    [improved]: 불필요 주석 제거

--- static/google-auth.js
+++ static/google-auth.js
@@ -1,14 +1,11 @@
 function init() {
   gapi.load('auth2', function () {
     window.$gapi = gapi
-
-    // renderButton()
   })
 }
 
 document.addEventListener('SiginOut', () => {
   var auth2 = gapi.auth2.getAuthInstance()
-  console.log(auth2)
 
   auth2.signOut().then(function () {
     console.log('User signed out.')
