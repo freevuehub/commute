54867a04f4ab92b6d18770cdfaae8af5fad569a4
freevue <hackish113@gmail.com>
2020-09-02 18:32:41 +0900

    [improved]: 깃허브 로그인을 위한 콜백 위치 고정 지정

---
+++ pages/github-callback.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>callback</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from '@vue/composition-api'
+import { AuthConstant } from '~/constant'
+
+export default defineComponent({
+  setup(_, context) {
+    onMounted(() => {
+      const { code } = context.root.$route.query
+
+      context.root.$store.dispatch(`auth/${AuthConstant.$Call.GitHubSigin}`, code)
+    })
+  },
+})
+</script>
