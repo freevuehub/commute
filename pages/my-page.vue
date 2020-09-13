f7034a317a23cbfa5eb9a8842f533d60537ca2e5
freevue <hackish113@gmail.com>
2020-09-13 21:33:15 +0900

    [UX/UI]: 마이 페이지 영역 기본틀 구성

--- pages/my-page.vue
+++ pages/my-page.vue
@@ -1,5 +1,50 @@
 <template>
   <v-container fluid>
+    <h2 class="mb-2">기본 정보</h2>
+    <v-card class="mb-5">
+      <v-card-text>
+        <row class="mb-3" title="출근 시간">
+          <span class="body-1 font-weight-bold">{{ item.startDate || 'N/A' }}</span>
+        </row>
+        <row class="mb-3" title="퇴근 시간">
+          <span class="body-1 font-weight-bold">{{ item.totalWorkTime || 'N/A' }}</span>
+        </row>
+        <row title="점심 시간">
+          <span class="body-1 font-weight-bold">{{ item.endDate || 'N/A' }}</span>
+        </row>
+      </v-card-text>
+    </v-card>
     <h2 class="mb-2">회사 정보</h2>
+    <v-card class="mb-5" dark>
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        src="https://lh3.ggpht.com/p/AF1QipMy3ma_xoft6AHOyzkoK3A4ZTU1tjtCVa7Xbp_c=s1024"
+      >
+        <v-card-title>{{ item.companyName }}</v-card-title>
+      </v-img>
+      <v-card-text>
+        <row title="주소">
+          <span class="address body-1 font-weight-bold">{{ item.companyAddress }}</span>
+        </row>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import { CommuteDetailRow } from '~/components'
+
+export default defineComponent({
+  components: {
+    row: CommuteDetailRow,
+  },
+  props: {
+    item: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+})
+</script>
