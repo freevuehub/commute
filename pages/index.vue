dbd5a410526b3fc5056cbe2d0a62fcbd37ca1cc1
freevue <hackish113@gmail.com>
2020-09-14 17:15:03 +0900

    [UX/UI]: pc 대응을 위한 메인 페이지 대시보드 반응형 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,43 @@
 <template>
   <v-container fluid>
-    <h2 class="mb-2">일일 현황</h2>
-    <today :item="state.mainData.todayData" />
-
-    <h2 class="mb-2">일주일 통계</h2>
-    <stats title="이번 주">
-      <span slot="sum">{{ state.mainData.weekTermSum }}</span>
-      <span slot="avg">{{ state.mainData.weekTermAvg }}</span>
-      <span slot="over">{{ state.mainData.weekOverTime }}</span>
-    </stats>
-
-    <h2 class="mb-2">일주일 상황판</h2>
-    <v-card class="py-5 px-2 mb-5">
-      <v-sparkline
-        :value="computed.weekBarValue"
-        :auto-draw-duration="300"
-        :gradient="['#71abbd']"
-        auto-line-width
-        type="bar"
-        auto-draw
-        :show-labels="true"
-        :label-size="12"
-        :labels="computed.weekBarLabels"
-      ></v-sparkline>
-    </v-card>
-
-    <h2 class="mb-2">월 통계</h2>
-    <stats title="이번 달">
-      <span slot="sum">{{ state.mainData.monthTermSum }}</span>
-      <span slot="avg">{{ state.mainData.monthTermAvg }}</span>
-      <span slot="over">{{ state.mainData.monthOverTime }}</span>
-    </stats>
+    <v-row>
+      <v-col cols="12">
+        <h2 class="mb-2">일일 현황</h2>
+        <today :item="state.mainData.todayData" />
+      </v-col>
+      <v-col cols="12" sm="6">
+        <h2 class="mb-2">일주일 통계</h2>
+        <stats title="이번 주">
+          <span slot="sum">{{ state.mainData.weekTermSum }}</span>
+          <span slot="avg">{{ state.mainData.weekTermAvg }}</span>
+          <span slot="over">{{ state.mainData.weekOverTime }}</span>
+        </stats>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <h2 class="mb-2">일주일 상황판</h2>
+        <v-card class="py-5 px-2 mb-5">
+          <v-sparkline
+            :value="computed.weekBarValue"
+            :auto-draw-duration="300"
+            :gradient="['#71abbd']"
+            auto-line-width
+            type="bar"
+            auto-draw
+            :show-labels="true"
+            :label-size="12"
+            :labels="computed.weekBarLabels"
+          ></v-sparkline>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <h2 class="mb-2">월 통계</h2>
+        <stats title="이번 달">
+          <span slot="sum">{{ state.mainData.monthTermSum }}</span>
+          <span slot="avg">{{ state.mainData.monthTermAvg }}</span>
+          <span slot="over">{{ state.mainData.monthOverTime }}</span>
+        </stats>
+      </v-col>
+    </v-row>
 
     <floating-button
       :disabled-start="!!state.mainData.todayData.startDate"
