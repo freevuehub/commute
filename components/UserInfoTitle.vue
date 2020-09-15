86c701ee581e845033b26e49659c1f3910c2e01e
freevue <hackish113@gmail.com>
2020-09-15 15:45:45 +0900

    [refactor]: 유저의 정보 타이틀 영역 컴포넌트 화

---
+++ components/UserInfoTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ isWork ? computed.workMessage : computed.joblessMessage }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import { useState, useComputed } from './user-info-title.fn'
+
+export default defineComponent({
+  props: {
+    isWork: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {
+    const state = useState()
+    const computed = useComputed(state)
+
+    return {
+      state,
+      computed,
+    }
+  },
+})
+</script>
