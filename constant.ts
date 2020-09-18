394cba2f05d8987e2d0a8a58867f5ae2581cc232
freevue <hackish113@gmail.com>
2020-09-18 10:22:38 +0900

    [refactor]: lint check

--- constant.ts
+++ constant.ts
@@ -1,61 +1,61 @@
 export const AuthConstant = {
-	$Set: {
-		Info: 'SET/USER/INFO',
-		Profile: 'SET/PROFILE',
-	},
-	$Call: {
-		Info: 'CALL/USER/INFO',
-		InfoPut: 'CALL/USER/INFO/PUT',
-		Profile: 'CALL/USER/PROFILE',
-		GitHubSigin: 'CALL/GITHUB/SIGNIN',
-	},
-	$Get: {
-		Info: 'GET/USER/INFO',
-		Profile: 'GET/PROFILE',
-	},
+  $Set: {
+    Info: 'SET/USER/INFO',
+    Profile: 'SET/PROFILE',
+  },
+  $Call: {
+    Info: 'CALL/USER/INFO',
+    InfoPut: 'CALL/USER/INFO/PUT',
+    Profile: 'CALL/USER/PROFILE',
+    GitHubSigin: 'CALL/GITHUB/SIGNIN',
+  },
+  $Get: {
+    Info: 'GET/USER/INFO',
+    Profile: 'GET/PROFILE',
+  },
 }
 
 export const MainConstant = {
-	$Set: {
-		MainData: 'SET/DATA',
-	},
-	$Call: {
-		MainData: 'CALL/DATA',
-	},
-	$Get: {
-		MainData: 'GET/DATA',
-	},
+  $Set: {
+    MainData: 'SET/DATA',
+  },
+  $Call: {
+    MainData: 'CALL/DATA',
+  },
+  $Get: {
+    MainData: 'GET/DATA',
+  },
 }
 
 export const SnackConstant = {
-	$Set: {
-		SnackStatus: 'SET/STATUS',
-	},
-	$Call: {
-		SnackStatus: 'CALL/STATUS',
-		Success: 'CALL/SUCCESS',
-		Error: 'CALL/ERROR',
-	},
-	$Get: {
-		SnackStatus: 'GET/STATUS',
-	},
+  $Set: {
+    SnackStatus: 'SET/STATUS',
+  },
+  $Call: {
+    SnackStatus: 'CALL/STATUS',
+    Success: 'CALL/SUCCESS',
+    Error: 'CALL/ERROR',
+  },
+  $Get: {
+    SnackStatus: 'GET/STATUS',
+  },
 }
 
 export const CommuteConstant = {
-	$Set: {
-		CommuteItem: 'SET/ITEM',
-		CommuteList: 'SET/LIST',
-		CommuteListTotal: 'SET/LIST/TOTAL',
-	},
-	$Call: {
-		CommutePost: 'CALL/POST',
-		CommutePut: 'CALL/PUT',
-		CommuteGetList: 'CALL/GET/LIST',
-		CommuteGetItem: 'CALL/GET/ITEM',
-	},
-	$Get: {
-		CommuteList: 'GET/LIST',
-		CommuteItem: 'GET/ITEM',
-		CommuteListTotal: 'GET/LIST/TOTAL',
-	},
+  $Set: {
+    CommuteItem: 'SET/ITEM',
+    CommuteList: 'SET/LIST',
+    CommuteListTotal: 'SET/LIST/TOTAL',
+  },
+  $Call: {
+    CommutePost: 'CALL/POST',
+    CommutePut: 'CALL/PUT',
+    CommuteGetList: 'CALL/GET/LIST',
+    CommuteGetItem: 'CALL/GET/ITEM',
+  },
+  $Get: {
+    CommuteList: 'GET/LIST',
+    CommuteItem: 'GET/ITEM',
+    CommuteListTotal: 'GET/LIST/TOTAL',
+  },
 }
