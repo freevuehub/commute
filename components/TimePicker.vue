dcca87df0899cb870ea3382bd5ee822feef5032f
freevue <hackish113@gmail.com>
2020-09-21 21:58:29 +0900

    [refactor]: 기능이 중복되는 부분 병합

---
+++ components/TimePicker.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-time-picker
+    color="primary"
+    :value="time"
+    format="ampm"
+    full-width
+    :min="min || null"
+    :max="max || null"
+    @input="onTimeInput"
+  />
+</template>
+
+<script lang="ts">
+import { SetupContext, defineComponent } from '@vue/composition-api'
+
+interface IProps {
+  time: string
+}
+
+export default defineComponent({
+  props: {
+    time: String,
+    min: String,
+    max: String,
+  },
+  model: {
+    prop: 'time',
+    event: 'input',
+  },
+  setup<IProps>(_: IProps, { emit }: SetupContext) {
+    const onTimeInput = (value: string) => {
+      emit('input', value)
+    }
+
+    return {
+      onTimeInput,
+    }
+  },
+})
+</script>
