307fc72d055ee884330837bedc5471573a1b619c
freevue <hackish113@gmail.com>
2020-09-23 11:11:06 +0900

    [improved]: 임시적인 내용 채움

--- pages/company/index.vue
+++ pages/company/index.vue
@@ -1,5 +1,58 @@
 <template>
   <v-container fluid>
-    company
+    <v-row>
+      <v-col cols="12">
+        <v-card :class="$round" elevation="10">
+          <v-card-title>회사 검색</v-card-title>
+          <v-card-text>
+            <search v-model="state.company" />
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card :class="$round" elevation="10">
+          <v-card-title>회사 등록 요청</v-card-title>
+          <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+            <v-card-text>
+              <v-text-field label="회사 이름" dense outlined></v-text-field>
+            </v-card-text>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@vue/composition-api'
+import { CompanyAutoSearchForm } from '~/components'
+
+export default defineComponent({
+  components: {
+    search: CompanyAutoSearchForm,
+  },
+  setup() {
+    const state = reactive({
+      company: {
+        id: 0,
+        ceoName: '',
+        companyAddress: '',
+        companyEsName: null,
+        companyLat: null,
+        companyLong: null,
+        companyName: '',
+        companyZipCode: 0,
+        contact: '',
+        establishmenDate: '',
+        webPage: null,
+        imgUrl: null,
+        industryName: '',
+      },
+    })
+
+    return {
+      state,
+    }
+  },
+})
+</script>
