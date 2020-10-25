965910a107b9d2ce7d8c42d3fca9b955c2077410
freevue <hackish113@gmail.com>
2020-10-25 21:19:22 +0900

    [improved]: lottie 메소드 체크

--- components/frames/LottieAni.vue
+++ components/frames/LottieAni.vue
@@ -1,11 +1,20 @@
 <template>
-  <lottie :path="path" />
+  <lottie
+    :path="path"
+    loop
+    auto-play
+    :speed="1"
+    :width="width"
+    :height="height"
+    @AnimControl="setAnimController"
+  />
 </template>
 
 <script lang="ts">
+import { defineComponent } from '@vue/composition-api'
 import LottieAnimation from 'lottie-vuejs/src/LottieAnimation.vue'
 
-export default {
+export default defineComponent({
   components: {
     lottie: LottieAnimation,
   },
@@ -14,6 +23,30 @@ export default {
       type: String,
       default: '',
     },
+    speed: {
+      type: Number,
+      default: 1,
+    },
+    width: {
+      type: Number,
+      default: 253,
+    },
+    height: {
+      type: Number,
+      default: 253,
+    },
   },
-}
+  setup() {
+    const setAnimController = (event) => {
+      console.log(event)
+      // setTimeout(() => {
+      // }, 1000)
+      event.setDirection(-1)
+    }
+
+    return {
+      setAnimController,
+    }
+  },
+})
 </script>
