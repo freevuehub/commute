edd03b9d5c5ec5125afd1b185e553ce689ba9b25
freevue <hackish113@gmail.com>
2020-11-02 21:09:19 +0900

    [new]: 회사 상세 페이지 추가

---
+++ pages/company/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    회사 상세
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
