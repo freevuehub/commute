c173dda725be1b848aa6ef901cd9d3035735f093
freevue <hackish113@gmail.com>
2020-11-11 21:29:43 +0900

    [new]: 휴가 api 연동 파일 준비

---
+++ API/vacation.ts
@@ -0,0 +1,13 @@
+// import { IResponseData, IUserProfile, IUserInfoPutPayload } from '@/types'
+// import endpoint from './endpoint.config'
+// import { AxiosGet, AxiosPut, AxiosPost } from './util'
+
+// export const getUserInfo = async (token: string = '') => {
+//   try {
+//     const response = await AxiosGet(endpoint.user.request.info(), token, {})
+
+//     return response
+//   } catch (err) {
+//     return err
+//   }
+// }
